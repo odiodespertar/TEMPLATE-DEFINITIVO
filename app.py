04e736dba92b1cb6cf5e38b9cd61f1e202c904f3
odiodespertar <odiodespertar@gmail.com>
2026-05-05 16:36:12 -0600

    1

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def gen_poligonos():
 # Busca esta línea dentro de gen_poligonos():
 <div class="poligono-bloque" style="margin-bottom: 25px; box-shadow: 0 10px 20px rgba(0,0,0,0.1), 0 6px 6px rgba(0,0,0,0.1); border-radius: 12px; overflow: hidden; background: white; border: 1px solid #e1e1e1; transform: translateZ(0);">            <table style="width: 100%; border-collapse: collapse;">
                 <thead>
-                    <tr style="background: linear-gradient(180deg, #888888 0%, #696969 100%); color: white; font-size: 12px; height: 35px;">
+                    <tr style="background: linear-gradient(168, 168, 168, 1); color: white; font-size: 12px; height: 35px;">
                         <th style="padding: 0 10px;">PLAN / RUTA</th>
                         <th>VOL. TOTAL</th>
                         <th style="width: 110px;"># ASIGNADAS</th>
