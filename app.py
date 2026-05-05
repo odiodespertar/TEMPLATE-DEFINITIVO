9a08ebccf76ca27154f7fd3f1535dbdd383825b8
odiodespertar <odiodespertar@gmail.com>
2026-05-05 17:01:31 -0600

    1

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def gen_poligonos():
     </tr>'''
     for i in range(1, 11):
         polys += f'''
-# Busca esta línea dentro de gen_poligonos():
+
 <div class="poligono-bloque" style="margin-bottom: 25px; box-shadow: 0 10px 20px rgba(0,0,0,0.1), 0 6px 6px rgba(0,0,0,0.1); border-radius: 12px; overflow: hidden; background: white; border: 1px solid #e1e1e1; transform: translateZ(0);">            <table style="width: 100%; border-collapse: collapse;">
                 <thead>
 <tr style="background: linear-gradient(180deg, #A9A9A9, #575757); color: white; font-size: 12px; height: 36px;">                        <th style="padding: 0 10px;">PLAN / RUTA</th>
