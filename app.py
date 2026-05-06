10d3e05590b8739cb7c874933d25541fb11defc5
odiodespertar <odiodespertar@gmail.com>
2026-05-06 15:49:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -223,6 +223,19 @@ body {{ font-family: sans-serif; background: #ffffff; padding: 14px; }}
         .meli-table td:first-child {{ border-radius: 12px 0 0 12px; }}
         .meli-table td:last-child {{ border-radius: 0 12px 12px 0; }}
 
+//////////
+/* Última fila de Disponibilidad de Flota */
+.meli-table tr:last-child td {{
+    height: auto !important; /* Permite que la celda crezca */
+    padding: 10px 5px !important; /* Da espacio arriba y abajo del texto */
+}}
+
+.meli-table tr:last-child .f-left {{
+    font-size: 22px !important; /* Tamaño de letra grande */
+    font-weight: 900 !important;
+    line-height: 1.2; /* Ajusta el espacio entre líneas */
+}}
+////////////
         
         #google-alert {{ 
             position: fixed; top: -100px; left: 50%; transform: translateX(-50%);
