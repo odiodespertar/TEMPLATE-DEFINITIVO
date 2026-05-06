1277c9f9999bd16dcd3979e84f5a9ba0d1f6adbc
odiodespertar <odiodespertar@gmail.com>
2026-05-06 14:52:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ app_html = f"""
 </head>
 
     <style>
-body {{ font-family: sans-serif; background: #ffffff; padding: 15px; }}
+body {{ font-family: sans-serif; background: #ffffff; padding: 14px; }}
 .meli-table {{ 
     border-collapse: separate; /* Cambiado para que se noten las sombras de celda */
     border-spacing: 0 8px;
