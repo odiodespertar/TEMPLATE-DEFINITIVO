8dafb2d7402bbbfea1a63819e7b90d27e0c07bcc
odiodespertar <odiodespertar@gmail.com>
2026-05-06 14:35:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ body {{ font-family: sans-serif; background: #f5f7f9; padding: 15px; }}
 /* El efecto Neomórfico en cada fila */
         .master-row {{
             border-radius: 9px;
-            box-shadow: 1px 1px 5px #ededed, -2px -2px 6px #FFFFFF;
+            box-shadow: 1px 1px 5px #ededed, -2px -2px 6px #efefef;
             transition: all 0.2s ease;
         }}
 
