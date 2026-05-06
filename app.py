8fdcc371c5115b467610ef6d10e94a597b99594f
odiodespertar <odiodespertar@gmail.com>
2026-05-06 16:11:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -392,7 +392,7 @@ html body .meli-table tbody tr:last-child {{
 /* Cambiar el color de la casilla OK cuando se selecciona */
 #body-plan input[type="checkbox"] {{
     accent-color: #FF00FF; /* Este es el Rosa Flúor (Magenta) */
-    transform: scale(1.3); /* Aprovechamos para hacerla un poquito más grande y fácil de picar */
+    transform: scale(2); /* Aprovechamos para hacerla un poquito más grande y fácil de picar */
     cursor: pointer;
 }}
     
