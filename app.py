c8b7816455b21a5efa8ff04ec261733318e416ab
odiodespertar <odiodespertar@gmail.com>
2026-05-05 18:46:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ app_html = f"""
 .meli-table tr:last-child td {{
     height: 20px !important; /* Ajusta este valor (ej. 15px, 20px) según lo que necesites */
     line-height: 18px !important;
-    font-size: 10px !important; /* Opcional: reducir un poco la letra si es muy delgada */
+    font-size: 14px !important; /* Opcional: reducir un poco la letra si es muy delgada */
     padding-bottom: 2px !important;
     padding-top: 2px !important;
 }}
