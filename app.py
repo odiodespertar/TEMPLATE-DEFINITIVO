7130d517ccc63d7e1487e75dd6432f8039f33bea
odiodespertar <odiodespertar@gmail.com>
2026-05-10 13:10:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,27 @@ st.markdown("""
 
 # --- DATOS BASE ---
 u_SDE = {"MOTO 3H": [25, 28], "CROWD 5H": [25, 28], "CROWD 5H EXT": [25, 28], "CROWD 3H": [25, 28]}
-u_PREC = {"EXT LARGE V MLP": [50, 50], "EXT LARGE V MLP H&B": [50, 50], "LARGE V MLP NEW": [50, 50], "SMALL V MLP NEW": [50, 50], "LARGE V MLP": [80, 85], "SMALL V MLP": [80, 85], "CAR MLP": [50, 50], "MOTO 3H": [35, 37], "MOTO 7H": [35, 37], "SMALL V NEW": [35, 37], "CROWD NEW": [35, 37], "CROWD 5H EXT": [35, 37], "SMALL V 5H": [35, 37], "CROWD 5H": [35, 37], "CROWD ZON EXT": [35, 37], "SMALL V 9H": [35, 37], "CROWD 8H": [35, 37],}
+
+
+# Diccionario con los dos servicios
+servicios_prec = {
+    "SMX5": {
+        "EXT LARGE V MLP": [50, 50], "EXT LARGE V MLP H&B": [50, 50], "LARGE V MLP NEW": [50, 50], 
+        "SMALL V MLP NEW": [50, 50], "LARGE V MLP": [80, 85], "SMALL V MLP": [80, 85], "CAR MLP": [50, 50]
+    },
+    "SMX11": {
+        "MOTO 3H": [35, 37], "MOTO 7H": [35, 37], "SMALL V NEW": [35, 37], "CROWD NEW": [35, 37], 
+        "CROWD 5H EXT": [35, 37], "SMALL V 5H": [35, 37], "CROWD 5H": [35, 37], "CROWD ZON EXT": [35, 37], 
+        "SMALL V 9H": [35, 37], "CROWD 8H": [35, 37]
+   }
+}
+        
+# Selector en la barra lateral para que elijas el service
+service_actual = st.sidebar.selectbox("Service para PREC", options=list(servicios_prec.keys()))
+
+# IMPORTANTE: Aquí asignamos los datos del service elegido a u_PREC
+u_PREC = servicios_prec[service_actual]
+        
 u_C1 = {
     "RENTAL E. LARGE": [120, 120], "RENTAL E. SMALL": [120, 120], "RENTAL LARGE": [120, 120], 
     "RENTAL SMALL": [120, 120], "LARGE V VAR(MLP)": [100, 100], "SMALL V VAR(MLP)":[80, 80],
