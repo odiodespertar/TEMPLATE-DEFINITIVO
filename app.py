ed1e19d1f5d66eb8a8fcf404331614e2a68294e0
odiodespertar <odiodespertar@gmail.com>
2026-05-10 00:27:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -865,6 +865,10 @@ info_operativa = {
             <p style='margin: 0;'><strong><span style="color: #ff8c00;">●</span> SMX5 AM3 - ⏰ 10:00 pm</strong></p>
         </div>
 
+        <div style='background: white; border-left: 6px solid #ff8c00; padding: 12px; border-radius: 8px; box-shadow: 2px 2px 5px rgba(0,0,0,0.1); color: #000; margin-bottom: 12px;'>
+            <p style='margin: 0;'><strong><span style="color: #ff8c00;">●</span> SMX6 AM3 - ⏰ 10:00 pm</strong></p>
+        </div>
+
         <h3 style='color: #000; margin-top: 25px;'>👉 OTROS RUTEOS PM2 (SDE)</h3>
         <hr style='border: 1px solid #808080; margin-bottom: 20px;'>
 
