c2e445113218a09a5f18280d6ea332b41730e8d6
odiodespertar <odiodespertar@gmail.com>
2026-05-10 13:38:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -460,6 +460,24 @@ html body .meli-table tbody tr:last-child {{
                         <th style="border-right: 0.5px solid #555; padding: 2px; font-size: 10px; background: linear-gradient(180deg, #222 0%, #000 100%); color: white; line-height: 1.2;">MAX</th>
                     </tr>
                 </thead>
+                <tbody id="body-3">{gen_master_rows(u_C2, 3)}</tbody>
+            </table>
+        </div>
+        <div id="tab-1" class="t-content" style="display:none;">
+            <table class="meli-table">
+                <thead>
+                    <tr style="background: linear-gradient(180deg, #444 0%, #111 100%); color: white;">
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px; text-shadow: 1px 1px 2px #000; box-shadow: inset 0 1px 0 rgba(255,255,255,0.1); line-height: 1.2;">UNIDAD</th>
+                        <th colspan="2" style="border-bottom: 0.5px solid #555; border-right: 0.5px solid #555; padding: 2px; font-size: 11px; letter-spacing: 1px; background: rgba(255,255,255,0.05); line-height: 1.2;">SPR</th>
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px; text-shadow: 1px 1px 2px #000; box-shadow: inset 0 1px 0 rgba(255,255,255,0.1); line-height: 1.2;">ORH</th>
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px; text-shadow: 1px 1px 2px #000; box-shadow: inset 0 1px 0 rgba(255,255,255,0.1); line-height: 1.2;">SCHED</th>
+                        <th rowspan="2" style="padding: 4px 8px; font-size: 11px; text-shadow: 1px 1px 2px #000; box-shadow: inset 0 1px 0 rgba(255,255,255,0.1); line-height: 1.2;">ME QUEDAN</th>
+                    </tr>
+                    <tr>
+                        <th style="border-right: 0.5px solid #555; padding: 2px; font-size: 10px; background: linear-gradient(180deg, #222 0%, #000 100%); color: white; line-height: 1.2;">MIN</th>
+                        <th style="border-right: 0.5px solid #555; padding: 2px; font-size: 10px; background: linear-gradient(180deg, #222 0%, #000 100%); color: white; line-height: 1.2;">MAX</th>
+                    </tr>
+                </thead>
                 <tbody id="body-1">{gen_master_rows(u_PREC, 1)}</tbody>
             </table>
         </div>
