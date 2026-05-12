4e794a687d3689ad1f1bdb9919eb6360eeb9b179
odiodespertar <odiodespertar@gmail.com>
2026-05-12 11:19:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1007,7 +1007,7 @@ info_operativa = {
             <p style='margin: 0;'><strong>👉👉 INDICACIONES</strong><br>
             - 📌 Origen + onway / si no especifican<br>
             - 👉 Schedule del día siguiente / apartado en archivo AMO<br>
-            - 👀 Revisar si mandan blancos<br>
+            - 👀 Revisar si mandan ids a agregar<br>
             - ✅ delimitación / ✅ dejar restricción para MLP /  ✅ dejar restricción para Crowd<br>
             - Revisar en qué polígonos acepta MLP para meterlas</p>
         </div>
