b33f7e8a9a413169a604258cc9c4880e32c3caef
odiodespertar <odiodespertar@gmail.com>
2026-05-15 14:48:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -510,7 +510,7 @@ html body .meli-table tbody tr:last-child {{
                     <tr style="background: linear-gradient(180deg, #333 0%, #1a1a1a 100%); color: white;">
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">UNIDAD</th>
                         <th colspan="2" style="border-bottom: 0.5px solid #555; border-right: 0.5px solid #555; padding: 2px; font-size: 11px;">SPR</th>
-                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/%</th>
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/ %</th>
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">SCHED</th>
                         <th rowspan="2" style="padding: 4px 8px; font-size: 11px;">ME QUEDAN</th>
                     </tr>
@@ -530,7 +530,7 @@ html body .meli-table tbody tr:last-child {{
                     <tr style="background: linear-gradient(180deg, #444 0%, #111 100%); color: white;">
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">UNIDAD</th>
                         <th colspan="2" style="border-bottom: 0.5px solid #555; border-right: 0.5px solid #555; padding: 2px; font-size: 11px;">SPR</th>
-                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/%</th>
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/ %</th>
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">SCHED</th>
                         <th rowspan="2" style="padding: 4px 8px; font-size: 11px;">ME QUEDAN</th>
                     </tr>
@@ -550,7 +550,7 @@ html body .meli-table tbody tr:last-child {{
                     <tr style="background: linear-gradient(180deg, #444 0%, #111 100%); color: white;">
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">UNIDAD</th>
                         <th colspan="2" style="border-bottom: 0.5px solid #555; border-right: 0.5px solid #555; padding: 2px; font-size: 11px;">SPR</th>
-                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/%</th>
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/ %</th>
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">SCHED</th>
                         <th rowspan="2" style="padding: 4px 8px; font-size: 11px;">ME QUEDAN</th>
                     </tr>
@@ -571,7 +571,7 @@ html body .meli-table tbody tr:last-child {{
                     <tr style="background: linear-gradient(180deg, #444 0%, #111 100%); color: white;">
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">UNIDAD</th>
                         <th colspan="2" style="border-bottom: 0.5px solid #555; border-right: 0.5px solid #555; padding: 2px; font-size: 11px;">SPR</th>
-                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/%</th>
+                        <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">ORH/ %</th>
                         <th rowspan="2" style="border-right: 0.5px solid #555; padding: 4px 8px; font-size: 11px;">SCHED</th>
                         <th rowspan="2" style="padding: 4px 8px; font-size: 11px;">ME QUEDAN</th>
                     </tr>
