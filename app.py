0da25959766751f84021baf19f90053cb560609b
odiodespertar <odiodespertar@gmail.com>
2026-05-15 13:34:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def gen_poligonos(data_target=None): # Usamos un nombre genérico para evitar er
         # --- LÓGICA DE NOMBRES CORREGIDA ---
         if (data_target == u_PREC) and (i-1) < len(nombres_prec):
             nombre_final = nombres_prec[i-1]
-        elif (data_target == u_PREC_SMX2) and (i-1) < len(nombres_smx2):
+        elif (data_target == u_PREC_SMX2) and (i-1) < len(nombres_smx2): 
             nombre_final = nombres_smx2[i-1]
         else:
             nombre_final = f"PLAN {i}"
