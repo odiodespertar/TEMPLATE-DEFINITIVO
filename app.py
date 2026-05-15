a80e4a0c80febfbeac11fa50bdc91eee5d328986
odiodespertar <odiodespertar@gmail.com>
2026-05-15 16:36:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -640,7 +640,7 @@ html body .meli-table tbody tr:last-child {{
             font-weight:bold;
             font-size:14px;
             padding:12px;
-            background:linear-gradient(180deg, #FFDAB9 0%, #F4A460 100%);
+            background:linear-gradient(180deg, #FF6347 0%, #F4A460 100%);
             color:white;
             user-select:none;
         ">
@@ -757,7 +757,7 @@ html body .meli-table tbody tr:last-child {{
             font-weight:bold;
             font-size:14px;
             padding:12px;
-            background:linear-gradient(180deg, #FFDAB9 0%, #F4A460 100%);
+            background:linear-gradient(180deg, #FF6347 0%, #F4A460 100%);
             color:white;
             user-select:none;
         ">
