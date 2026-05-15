c3fa58b6785fb00a087f4cb5afc865e06f837771
odiodespertar <odiodespertar@gmail.com>
2026-05-15 13:56:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -947,9 +947,9 @@ html body .meli-table tbody tr:last-child {{
             
             if(sch > 0) {{
                 row.style.background = "white"; row.style.color = "black";
-                fs.style.background = "#e3defa"; 
-                mi.style.background = "#def3ed"; mi.style.color = "#008B8B"; mi.style.fontWeight = "bold";
-                ma.style.background = "#def3ed"; ma.style.color = "#008B8B"; ma.style.fontWeight = "bold";
+                fs.style.background = "#fcfbc7"; 
+                mi.style.background = "#f5ffff"; mi.style.color = "#008B8B"; mi.style.fontWeight = "bold";
+                ma.style.background = "#f5ffff"; ma.style.color = "#008B8B"; ma.style.fontWeight = "bold";
             }} else {{
                 row.style.background = "#ebebeb"; row.style.color = "#969696";
                 fs.style.background = "#ebebeb"; 
