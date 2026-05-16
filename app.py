fa76faca1150a46248403f2d613843374328b845
odiodespertar <odiodespertar@gmail.com>
2026-05-16 05:22:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -787,7 +787,7 @@ style="text-align:center; color:#00BFFF;">
             font-weight:bold;
             font-size:14px;
             padding:12px;
-            background:linear-gradient(180deg, #9370D8 0%, #6495ED 100%);
+            background:linear-gradient(180deg, #6065f7 0%, #6495ED 100%);
             color: #ffffff;
             user-select:none;
         ">
