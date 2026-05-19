6bfaee9d2a75158f670e4259a8afffd3903420f7
odiodespertar <odiodespertar@gmail.com>
2026-05-19 15:08:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1004,8 +1004,7 @@ html body .meli-table tbody tr:last-child {{
     </div>
 </div>
 
-
-<div style="padding-bottom: 5px; display: flex; gap: 6px; align-items: center;"> 
+<div style="padding: 10px 0; width: 100%;">
     <button onclick="distribuirAutomatico()" 
     style="cursor:pointer; background: #FF00FF; color: white; border: none; font-size: 12px; padding: 6px 12px; border-radius: 4px; font-weight: bold; box-shadow: 0 3px 0 #b300b3; transition: all 0.05s; outline: none;"
     onmousedown="this.style.transform='translateY(2px)'; this.style.boxShadow='0 1px 0 #b300b3';"
@@ -1013,7 +1012,6 @@ html body .meli-table tbody tr:last-child {{
     onmouseleave="this.style.transform='translateY(0px)'; this.style.boxShadow='0 3px 0 #b300b3';">
     ⚡ AUTO-CALCULAR
 </button>
-
 
 <!-- COLUMNA IZQUIERDA -->
 <!-- PLANNERS -->
