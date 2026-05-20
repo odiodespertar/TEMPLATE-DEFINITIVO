13db61016e96ecc2a9e72bfb00cc5df48385fd9b
odiodespertar <odiodespertar@gmail.com>
2026-05-20 14:55:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1099,20 +1099,42 @@ html body .meli-table tbody tr:last-child {{
 
 
     function updateFleetFloat() {{
+
     let html = "";
+
     document.querySelectorAll('#body-' + currentTab + ' tr').forEach(row => {{
+
         let name = row.querySelector('.edit-name')?.innerText.trim();
-        let stock = parseInt(row.querySelector('.f-stock')?.innerText) || 0;
-        let left = parseInt(row.querySelector('.f-left')?.innerText) || 0;
+
+        let stock =
+            parseInt(row.querySelector('.f-stock')?.innerText) || 0;
+
+        let left =
+            parseInt(row.querySelector('.f-left')?.innerText) || 0;
+
         if(name && stock > 0){{
-            let isCar = name.toLowerCase().includes("car") || name.toLowerCase().includes("híbrida");
-            let colorCategoria = isCar ? "#20B2AA" : "#FF00FF";
+
+            let color =
+                left <= 0
+                ? "#ff6b6b"
+                : "#00ff99";
+
             html += `
-                <div style="display:flex; justify-content:space-between; margin-bottom:4px;">
-                    <span style="color: white;">${name}</span>
-                    <span style="color: ${colorCategoria}; font-weight: bold;">
+                <div style="
+                    display:flex;
+                    justify-content:space-between;
+                    margin-bottom:4px;
+                ">
+
+                    <span>${{name}}</span>
+
+                    <span style="
+                        color:${{color}};
+                        font-weight:bold;
+                    ">
                         ${{left}}/${{stock}}
                     </span>
+
                 </div>
             `;
         }}
@@ -1122,6 +1144,7 @@ html body .meli-table tbody tr:last-child {{
 
     document.getElementById('fleet-float-body').innerHTML = html;
 }}
+
 
 
     function showTab(n, btn) {{
