9f875e1fd5aab53c4a5155e702a63c2edcfd85e8
odiodespertar <odiodespertar@gmail.com>
2026-05-28 00:16:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def gen_poligonos(data_target=None):
                                 <button style="{btn_s}" onclick="stepVal(this, 1, 'u')">+</button>
                             </div>
                         </td>
-                        <td class="spr-real-cell" style="background: #FFFFFF; border: 0.5px solid #696969; padding: 2px; width: 100px; min-width: 135px; max-width: 100px;">
+                        <td class="spr-real-cell" style="background: #FFFFFF; border: 0.5px solid #696969; padding: 2px; width: 90px; min-width: 90px; max-width: 90px;">
                             <div style="{div_flex}">
                                 <button style="{btn_s}" onclick="stepVal(this, -1, 's')">-</button>
                                 <span contenteditable="true" class="spr-real-val" oninput="manualEdit(this)" style="{span_num_spr}">0</span>
