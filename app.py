eab315d942ab6b2bdc9a8dd5d5ab7d0de5f66ea9
odiodespertar <odiodespertar@gmail.com>
2026-05-28 04:41:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1817,6 +1817,9 @@ let totalCarSchedule = 0;
         // Dibujamos los totales abajo
         html += `
 
+            <div style="margin-top: 15px; padding-top: 10px; border-top: 2px solid #555;"> 
+
+
 <div style="display:flex; justify-content:space-between; color: #11bfae; font-weight: 800; font-size: 13px;">
     <span>TOTAL CAR (sched):</span> <span>${{totalCarSchedule}}</span>
 </div>
