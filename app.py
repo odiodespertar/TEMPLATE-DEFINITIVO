3ada55e46635742cf1c55c33fff456ec1f1bcc66
odiodespertar <odiodespertar@gmail.com>
2026-05-29 06:18:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ app_html = f"""
         
         /* Efecto de iluminación al pasar el mouse por las filas */
         tr.master-row:hover, tr.calc-row:hover {{
-            background-color: #135b83 !important;
+            background-color: #ffffff !important;
             box-shadow: inset 0 0 10px rgba(0,0,0,0.05);
             cursor: default;
         }}
