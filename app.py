d59affb4108154949fb3ed988c53a7684c239687
odiodespertar <odiodespertar@gmail.com>
2026-05-29 03:48:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -669,7 +669,7 @@ html body .meli-table tbody tr:last-child {{
     </button>
 
     <button class="filter-btn" onclick="filterRows(false)" 
-        style="cursor:pointer; background: #808080; color:white; border:none; font-size:12px; padding:6px 12px; border-radius:4px; font-weight:bold; box-shadow: 0 3px 0 #292929; transition: all 0.05s; outline: none;">
+        style="cursor:pointer; background: #808080; color:white; border:none; font-size:12px; padding:6px 12px; border-radius:4px; font-weight:bold; box-shadow: 0 3px 0 #454545; transition: all 0.05s; outline: none;">
         TODAS
     </button>
 </div>
