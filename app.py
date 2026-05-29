85a521fc3811baf332ab29c3aff25360f6c652a0
odiodespertar <odiodespertar@gmail.com>
2026-05-29 09:07:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -516,7 +516,7 @@ html body .meli-table tbody tr:last-child {{
     left: 14px !important;  /* Antes decía 'right' */
     right: auto !important; /* IMPORTANTE: Desactiva el derecho */ 
 
-    top: 270px !important;
+    top: 260px !important;
 
     width: 220px;
 
