ef4e75eb13e4c6640dde384bb095386ed04f1748
odiodespertar <odiodespertar@gmail.com>
2026-05-29 06:27:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ def gen_poligonos(data_target=None):
                 </thead>
                 <tbody>
                     <tr class="calc-row"> 
-                        <td rowspan="5" contenteditable="true" style="background: #D3D3D3; font-weight:bold; text-align:center; border: 1px solid #135b83; padding: 5px; color:#333;">{nombre_final}</td>
+                        <td rowspan="5" contenteditable="true" style="background: #a5b9c4; font-weight:bold; text-align:center; border: 1px solid #135b83; padding: 5px; color:#333;">{nombre_final}</td>
                         <td rowspan="5" contenteditable="true" class="v-total-val" oninput="recalc()" style="color: #808080; font-weight: bold; font-size: 18px; text-align: center; border: 1px solid #135b83; padding: 5px;">0</td>
                         <td class="u-manual-cell" style="background: #F5F5DC; border: 0.5px solid #135b83; padding: 2px; width: 105px; min-width: 105px; max-width: 105px;">
                             <div style="{div_flex}">
