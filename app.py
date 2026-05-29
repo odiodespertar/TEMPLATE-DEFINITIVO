786d42380997338290e796c73eb5049dab72a7c9
odiodespertar <odiodespertar@gmail.com>
2026-05-29 03:33:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def gen_poligonos(data_target=None):
                 <tbody>
                     <tr class="calc-row"> 
                         <td rowspan="5" contenteditable="true" style="background: #D3D3D3; font-weight:bold; text-align:center; border: 1px solid #696969; padding: 5px; color:#333;">{nombre_final}</td>
-                        <td rowspan="5" contenteditable="true" class="v-total-val" oninput="recalc()" style="color: #FFA500; font-weight: bold; font-size: 18px; text-align: center; border: 1px solid #696969; padding: 5px;">0</td>
+                        <td rowspan="5" contenteditable="true" class="v-total-val" oninput="recalc()" style="color: #008080; font-weight: bold; font-size: 18px; text-align: center; border: 1px solid #696969; padding: 5px;">0</td>
                         <td class="u-manual-cell" style="background: #F5F5DC; border: 0.5px solid #696969; padding: 2px; width: 105px; min-width: 105px; max-width: 105px;">
                             <div style="{div_flex}">
                                 <button style="{btn_s}" onclick="stepVal(this, -1, 'u')">-</button> 
