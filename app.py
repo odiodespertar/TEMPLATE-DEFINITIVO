4f2396bcdf3ccc91181c2cfd9b94f8d7f1e5212d
odiodespertar <odiodespertar@gmail.com>
2026-05-29 08:55:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1633,7 +1633,7 @@ let totalCarSchedule = 0;
 
                 html += `
                     <div style="display:flex; justify-content:space-between; margin-bottom:4px; font-size: 14px;">
-                        <span style="color: white;">${{name}}</span>
+                        <span style="color: #FFFFE0;">${{name}}</span>
                         <span style="color: ${{colorCategoria}}; font-weight: bold;">
                             ${{left}}/${{stock}}
                         </span>
