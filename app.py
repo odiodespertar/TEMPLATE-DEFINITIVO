a4555f4c342dc9d9ffeea9a5e85b5a6de48d2e05
odiodespertar <odiodespertar@gmail.com>
2026-05-29 23:56:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-import streamlit as st
+import streamlit as st 
 from streamlit.components.v1 import html      
 st.set_page_config(page_title="Monitor Logístico - Liliana García", layout="wide", initial_sidebar_state="expanded")
 
