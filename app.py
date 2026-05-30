0b00da0d48bec2aa928d0fa7de658c9f0966e9cd
odiodespertar <odiodespertar@gmail.com>
2026-05-29 18:51:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -520,6 +520,8 @@ html body .meli-table tbody tr:last-child {{
     /* Fondo: Tornasol de Durazno, Azul Cielo y Amarillo Crema */
     background: linear-gradient(135deg, 
         rgba(255, 218, 185, 0.4),  /* Durazno pastel */
+        rgba(251, 170, 153, 0.4),  /* Durazno pastel */
+        rgba(173, 216, 230, 0.4),  /* Azul cielo pastel */
         rgba(255, 255, 224, 0.4)   /* Amarillo crema pastel */
     );
     
