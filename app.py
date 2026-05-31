7dd58e9ec908072400fe48a428aac210af5bd054
odiodespertar <odiodespertar@gmail.com>
2026-05-31 05:13:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -539,8 +539,8 @@ html body .meli-table tbody tr:last-child {{
         inset -2px -2px 5px rgba(0,0,0,0.5);     
     
     /* Vidrio esmerilado */
-    backdrop-filter: blur(20px); 
-    -webkit-backdrop-filter: blur(20px);
+    backdrop-filter: blur(22px); 
+    -webkit-backdrop-filter: blur(22px);
     
     max-height: 75vh;
     overflow-y: auto;
