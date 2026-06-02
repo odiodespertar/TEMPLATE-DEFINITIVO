22239e95818afa8ffbc85e91ab564b48747b38eb
odiodespertar <odiodespertar@gmail.com>
2026-06-02 04:02:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -812,7 +812,7 @@ html body .meli-table tbody tr:last-child {{
             
                 <button id="toggle-tools-btn" onclick="toggleTools()" 
         style="cursor:pointer; 
-               background:#696969 !important; 
+               background:#A9A9A9 !important; 
                background-image: none !important; 
                box-shadow: none !important; 
                color: #ffffff !important; 
