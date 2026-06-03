dde20ac15247d3b62f52056806a40e0a4bc1d50b
odiodespertar <odiodespertar@gmail.com>
2026-06-03 14:16:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -933,12 +933,6 @@ html body .meli-table tbody tr:last-child {{
 
 
 
-
-
-   
-
-
-
     function showTab(n, btn) {{
         currentTab = n;
     // Oculta todo el contenido de polígonos y todas las tablas
@@ -1881,6 +1875,47 @@ actualizarDosPorciento();
 // ==============================================================================
 
 
+
+
+dragElement(document.getElementById("fleet-float"));
+// o dragElement(document.getElementById("panel-flota"));
+
+function dragElement(elmnt) {{
+
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {{
+        e = e || window.event;
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }}
+
+    function elementDrag(e) {{
+        e = e || window.event;
+        e.preventDefault();
+
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }}
+
+    function closeDragElement() {{
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }}
+}}
 
 
     
