8e4e5efa095dfff98f5f4c9f4ff51aca1ff9b0b0
odiodespertar <odiodespertar@gmail.com>
2026-06-05 14:27:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Monitor Logístico - Liliana García", layout="wi
 
 
 
-# CSS para diseño limpio
+# CSS para diseño limpio 
 st.markdown("""
     <style>
     .block-container {padding: 0rem !important;}
