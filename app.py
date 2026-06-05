8e13d4e4463531b4ce0d709e95f42decb8249811
odiodespertar <odiodespertar@gmail.com>
2026-06-05 17:15:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -260,6 +260,7 @@ def gen_poligonos(data_target=None):
                         <td style="border: 0.5px solid #135b83; padding: 2px;">
                             <select class="s-type" onchange="resetRow(this)" style="{select_style}">
                                 <option>Seleccionar...</option>
+                                {html_opciones}
                             </select>
                         </td>
                         <td style="width: 45px; min-width: 45px; max-width: 45px; text-align: center; border: 0.5px solid #135b83;"><input type="checkbox" class="ok-check" style="transform: scale(1.7); accent-color: #9ACD32; cursor: pointer;"></td>
@@ -1482,6 +1483,7 @@ document.querySelectorAll('#polys-' + tabId + ' .poligono-bloque').forEach(bl =>
             bl.querySelectorAll('.s-type').forEach(s => {{
                 let cur = s.value; 
                 let opt = '<option value="">Seleccionar...</option>';
+
                 
                 Object.keys(fleet).forEach(k => {{
                     let nameLower = k.toLowerCase();
