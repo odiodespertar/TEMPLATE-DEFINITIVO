46cc25ad6fd98caa4a85fc4d21c59115646cce59
odiodespertar <odiodespertar@gmail.com>
2026-06-09 01:37:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -298,7 +298,23 @@ def gen_poligonos(data_target=None):
 
 <div style="font-size:13px;font-weight:bold;color:#135b83;">
     Nodos:
-    <span class="nodos-val" contenteditable="true">0</span>
+    <span class="nodos-val"
+      contenteditable="true"
+      style="
+        display:inline-block;
+        min-width:28px;
+        text-align:center;
+        border:none;
+        border-radius:4px;
+        background:#ededed;
+        font-size:18px;
+        font-weight:bold;
+        color:#FF6347;
+        padding:0 4px;
+        margin-left:3px;
+      ">
+    0
+</span>
 </div>
 '''
 
