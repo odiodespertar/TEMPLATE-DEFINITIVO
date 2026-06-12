75cc831799fd508376932996c251b7dccc67ba91
odiodespertar <odiodespertar@gmail.com>
2026-06-12 12:59:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1074,35 +1074,23 @@ body.excel-view .spr-real-val{{
 
         
        
-        <div id="tab-2" class="t-content">
+<div id="tab-2" class="t-content">
        <table class="meli-table" style="width: 100%; table-layout: fixed; border-collapse: collapse;">
         <thead>
             <tr style="background: linear-gradient(180deg, #0a2e42 0%, #135b83 100%); color: white;">
                 <th style="border-right: 0.5px solid #135b83; padding: 4px 8px; font-size: 14px; color: #0c3a54 !important;">UNIDAD</th>
                 <th style="border-right: 0.5px solid #135b83; padding: 2px; font-size: 11px; color: #0c3a54 !important; width: 45px;">SPR MIN</th>
                 <th style="border-right: 0.5px solid #135b83; padding: 2px; font-size: 11px; color: #0c3a54 !important; width: 45px;">SPR MAX</th>
-<th style="
-    border-right:0.5px solid #135b83;
-    padding:4px 8px;
-    font-size:11px;
-    color: #0c3a54 !important;
-    width:60px;">
-    SCHEDULE
-</th>
+                
+                <th style="border-right: 0.5px solid #135b83; padding: 4px 8px; font-size: 11px; color: #0c3a54 !important; width: 60px;">ORH</th>
 
-<th style="
-    border-right:0.5px solid #135b83;
-    padding:4px 8px;
-    font-size:11px;
-    color: #0c3a54 !important;
-    width:50px;">
-    DELTA
-</th>
-</tr>
-
+                <th style="border-right: 0.5px solid #135b83; padding: 4px 8px; font-size: 11px; color: #0c3a54 !important; width: 75px;">OCUPACIÓN</th>
+            </tr>
         </thead>
+
+        
         <tbody id="body-2">{gen_master_rows(u_C1, 2)}</tbody>
-                  <tfoot class="fila-total"> 
+        <tfoot class="fila-total">
                   <tr class="fila-total">
     <td style="border:none;"></td>
     <td colspan="3" style="padding:6px; text-align:right;">
