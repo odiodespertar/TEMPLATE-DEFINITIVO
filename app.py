4950d3719a736cbc248e3d5dfd12da3a75a81ab6
odiodespertar <odiodespertar@gmail.com>
2026-06-23 04:20:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1466,21 +1466,7 @@ body.excel-view .spr-real-val{{
 </tr>
 
 
-<tr class="fila-total">
-    <td style="border:none;"></td>
-    <td colspan="3" style="padding:6px; text-align:right;">
-        TOTAL RUTEADAS
-    </td>
-    <td id="total-ruteadas-4"
-        style="
-            text-align:center;
-            color:#FF8C00;
-            font-size:16px;
-            font-weight:bold;
-        ">
-        0
-    </td>
-</tr>
+
 
 
 
