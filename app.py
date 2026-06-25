7e11e719bfaeef1871408f1c5eec1aecc403c053
odiodespertar <odiodespertar@gmail.com>
2026-06-25 07:43:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4697,7 +4697,7 @@ body:not(.tab-2) #excel-btn {{
 </div>
 
 <script>
-    const allData = {info_operativa}; 
+    const allData = {{info_operativa}}; 
     function changeTab(e, name) {{
         document.getElementById('visor').innerHTML = allData[name];
         let btns = document.getElementsByClassName('tab-btn');
