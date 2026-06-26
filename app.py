0f0c83a8b887878e6a3f39332aa5b359531ee6fd
odiodespertar <odiodespertar@gmail.com>
2026-06-26 08:00:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -609,7 +609,7 @@ app_html = f"""
 
         .cont-name {{
             font-weight: normal;
-            color: #F0E68C;
+            color: #D3D3D3;
             white-space: nowrap;
             overflow: hidden;
             text-overflow: ellipsis;
@@ -3602,7 +3602,7 @@ actualizarRelojRuteos();
 
                 htmlInyeccion += `
                     <div class="cont-item" style="display: flex; justify-content: space-between; align-items: center; border-bottom: 1px solid rgba(255,255,255,0.1); padding: 4px 0;">
-                        <div class="cont-name" style="font-weight: normal; color: #F0E68C; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 110px;" title="${{nombreUnidad}}">${{nombreUnidad}}</div>
+                        <div class="cont-name" style="font-weight: normal; color: #D3D3D3; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 110px;" title="${{nombreUnidad}}">${{nombreUnidad}}</div>
                         <div class="cont-vals" style="font-family: monospace; font-weight: bold; text-align: right;">
                             <span style="color:${{colorDelta}};" title="Disponibles">${{deltaRestante}}</span>
                             <span style="color:#ffffff; font-size:14px;"> | ORH:${{orhVal}} | %:${{ocupVal}}</span>
