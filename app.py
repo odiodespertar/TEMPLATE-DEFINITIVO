f2738980dc60495e046b14fc47a00c9ae4d35989
odiodespertar <odiodespertar@gmail.com>
2026-06-26 02:11:45 -0600

    Change background color of manual cell in table

--- app.py
+++ app.py
@@ -309,7 +309,7 @@ def gen_poligonos(data_target=None):
     
     fila_inner = f'''
     <tr class="calc-row">
-        <td class="u-manual-cell" style="background: #fff8b8; border: 0.6px solid #25282b; padding: 2px; width: 105px; min-width: 105px; max-width: 105px;">
+        <td class="u-manual-cell" style="background: #dcdcdc; border: 0.6px solid #25282b; padding: 2px; width: 105px; min-width: 105px; max-width: 105px;">
             <div style="{div_flex}">
                 <button style="{btn_s}" onclick="stepVal(this, -1, 'u')">-</button>
                 <span contenteditable="true" class="u-manual" oninput="manualEdit(this)" style="{span_num_u}color: #25282b !important;">0</span>
