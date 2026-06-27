925083347e024dfd8141b363f4e17265baa53f27
odiodespertar <odiodespertar@gmail.com>
2026-06-26 19:00:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -982,7 +982,7 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
         top: 25px; 
         right: 25px; 
         z-index: 99999999; /* Botón en la capa más alta */
-        background: #FF00FF; 
+        background: #aa14c4; 
         color: white; 
         border: 1px solid #333; 
         padding: 8px 12px; 
