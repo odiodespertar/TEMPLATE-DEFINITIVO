6ab07a135e4ac5633d09857931e3b4ebc2ecbce5
odiodespertar <odiodespertar@gmail.com>
2026-06-27 05:44:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1520,7 +1520,7 @@ USADAS
     background-image: none !important; 
     box-shadow: none !important; 
     border: none !important;
-    color: #008B8B; 
+    color: #20B2AA; 
     padding: 10px; 
     border-radius: 6px; 
     text-align: center; 
