1048237f861d4370744fc19a9baad5df02b78cdc
odiodespertar <odiodespertar@gmail.com>
2026-06-27 05:56:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1146,7 +1146,7 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
             <div style="font-size: 10px; font-weight: bold; color: #0861c7;">MLP</div>
             <div id="val-mlp-rute-2" style="font-size: 14px; font-weight: bold;">0</div>
         </div>
-        <div style="background: #88b6b8; padding: 8px; border-radius: 5px; border: 1px solid #20B2AA; text-align: center; width: 100px;">
+        <div style="background: #c6f7f3; padding: 8px; border-radius: 5px; border: 1px solid #20B2AA; text-align: center; width: 100px;">
             <div style="font-size: 10px; font-weight: bold; color: #d021eb;">RENTAL</div>
             <div id="val-rental-rute-2" style="font-size: 14px; font-weight: bold;">0</div>
         </div>
