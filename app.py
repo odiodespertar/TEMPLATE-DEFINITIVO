059c49cd2f97715d35b50ed97ec878477ac72c69
odiodespertar <odiodespertar@gmail.com>
2026-06-28 16:21:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1172,7 +1172,7 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
 
 <div id="fleet-sticky" class="fleet-normal">
   <div id="fleet-drag-handle">
-    PUEDES MOVERME
+   
 
     <button id="fleet-toggle-btn"
       onclick="console.log('CLICK BOTON FLOTAR/NORMAL'); toggleFleetFloating();"
