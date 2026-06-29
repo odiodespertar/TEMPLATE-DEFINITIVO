e778304294cb318d81319324cf91842452374d3b
odiodespertar <odiodespertar@gmail.com>
2026-06-28 19:02:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1179,9 +1179,9 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
    
 
     <button id="fleet-toggle-btn"
-      onclick="console.log('CLICK BOTON FLOTAR/NORMAL (da enter)'); toggleFleetFloating();"
+      onclick="console.log('CLICK BOTON FLOTAR/NORMAL'); toggleFleetFloating();"
       style="float:center; cursor:pointer; border:none; background:#25282b; color:white; padding:3px 8px; border-radius:6px; font-weight:bold;">
-      FLOTAR (da clic)
+      FLOTAR
     </button>
   </div>
 
