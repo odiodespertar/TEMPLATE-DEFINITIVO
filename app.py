4534d75af5dd5f96b37068fcefd9488ed6e2e87b
odiodespertar <odiodespertar@gmail.com>
2026-07-15 08:34:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1629,7 +1629,7 @@ USADAS
     border-radius: 6px; 
     text-align: center; 
     font-weight: bold; 
-    margin-bottom: 10px;">
+    margin-bottom: 50px !important;">
     📋 PLANIFICACIÓN POR POLÍGONOS
 </div>
         
