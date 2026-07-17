7eb8f6f173cc7cd393aab17d0c44b0b17bc1e059
odiodespertar <odiodespertar@gmail.com>
2026-07-17 03:09:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -230,12 +230,12 @@ def gen_master_rows(data_dict, table_id):
                 <td contenteditable="true"
                     class="edit-orh"
                     oninput="recalc()"
-                    style="text-align:center; border:0.2px solid #25282b; width:45px; background:#ffffff; color:#008000;">
+                    style="text-align:center; border:0.2px solid #25282b; width:45px; background:#ffffff; color:#6495ED;">
                     0
                 </td>
 
                 <td class="orh-hora"
-                    style="text-align:center; border:0.2px solid #25282b; width:60px; background:#f5f5f5; color:#008000; font-weight:bold;">
+                    style="text-align:center; border:0.2px solid #25282b; width:60px; background:#f5f5f5; color:#6495ED; font-weight:bold;">
                     00:00 hs
                 </td>
 
@@ -2013,7 +2013,7 @@ function actualizarHoraMinuto(celda){{
 
     if(hm){{
         // 🔥 CAMBIO DE COLOR AQUÍ: Forzamos el color naranja/café llamativo (#d97706) en el texto nativo
-        hm.style.color = "#008000";
+        hm.style.color = "#6495ED";
         
         hm.innerText =
             String(horas).padStart(2,"0") +
