fac7edadde58524591438e217250509ff303979c
odiodespertar <odiodespertar@gmail.com>
2026-07-16 23:04:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1147,9 +1147,9 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
             <h4 style="margin: 0 0 10px 0; color: #808080; font-size: 14px; font-weight: bold;">Prioridades SJA1 C1</h4>
             <ul style="padding-left: 20px; margin: 0; line-height: 1.4; font-size: 13px; color: black;">
                 <li>🟢 Local ➤ <b>Rentals Electric = meto todas</b></li> 
-                <li>🟢 Local ➤ <b>Rentals Large = si meto 3.5, delivery y H&B, quito 2)</b></li>
-                <li>🟢 Local ➤ <b>Rentals Large = si meto 3.5 y delivery, quito 1)</b></li>
-                <li>🟢 Local ➤ Truck 3.5 MLP y Delivery Large van (ambas dedicadas-3 paradas), H&B (bulk), MLP y crowd.</li>
+                <li>🟢 Local ➤ <b>Si meto 3.5, delivery y H&B, quito 2)</b></li>
+                <li>🟢 Local ➤ <b>Si meto 3.5 y delivery, quito 1)</b></li>
+                <li>🟢 Local ➤ <b>Truck 3.5 MLP (2 paradas), H&B (bulk), Delivery Large van (3 paradas)</b>, MLP y crowd.</li>
                 <li>🟣 Planes foráneos ➤ MLP (nodo=híbrida) ➡️ Solo Xico/Tuzamapa ➤ MLP y Crowd.</li>
                 <li>🔵 EJA1-SP ➤ Media milla-ruteo fake.</li>
                 <li>🟤 Alchichica ND-AM0 ➤ 2 unidades Small Van MLP/330 min = 65 ids.</li>
