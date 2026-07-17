6ec86efd4df78cf8f7d9418d5b42e2e404696c93
odiodespertar <odiodespertar@gmail.com>
2026-07-17 13:00:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1420,7 +1420,7 @@ USADAS
 <tr class="fila-total">
     <td style="border:none;"></td>
     <td colspan="6" style="padding:6px; text-align:right;">
-        🚛 TOTAL CAR RUTEADAS
+        🚛 TOTAL RUTEADAS
     </td>
     <td id="total-car-real-1"
         style="text-align:center; color:#3CB371; font-size:16px; font-weight:bold;">
@@ -1473,7 +1473,7 @@ USADAS
 <tr class="fila-total">
     <td style="border:none;"></td>
     <td colspan="6" style="padding:6px; text-align:right;">
-        🚛 TOTAL CAR RUTEADAS
+        🚛 TOTAL RUTEADAS
     </td>
     <td id="total-car-real-5"
         style="text-align:center; color:#3CB371; font-size:16px; font-weight:bold;">
@@ -1521,7 +1521,7 @@ USADAS
 <tr class="fila-total">
     <td style="border:none;"></td>
     <td colspan="3" style="padding:6px; text-align:right;">
-        🚛 TOTAL CAR RUTEADAS
+        🚛 TOTAL RUTEADAS
     </td>
     <td id="total-car-real-4"
         style="text-align:center; color:#3CB371; font-size:16px; font-weight:bold;">
