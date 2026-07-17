fb8f230ab0f321d0a289191efd2966375e3b250c
odiodespertar <odiodespertar@gmail.com>
2026-07-17 01:40:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ def gen_master_rows(data_dict, table_id):
                     style="text-align:center; border:0.2px solid #25282b; width:70px; background:#ffffff; color:#25282b;">
                     0
                 </td>
-                ''''
+                '''
 
 
                 
