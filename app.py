fdbed19994bda9acc5df4655cea962c3e8404409
odiodespertar <odiodespertar@gmail.com>
2026-08-06 07:17:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1928,7 +1928,7 @@ body.excel-view .poligono-bloque th:nth-child(7) {{ width: 45px !important; }} /
     <select id="ciclo-selector" onchange="cambiarCiclo(this.value)" style="
         background: #FF00FF;
         color: #ffffff;
-        border: 1px solid #4682B4;
+        border: 1px solid #242526;
         padding: 8px 15px;
         border-radius: 4px;
         font-size: 14px;
